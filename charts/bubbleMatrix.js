95ca5ae853f8081627cd70ce8d4e330a67ecfaca
Andrew Stretton <astretton@dee.sgc.ox.ac.uk>
2014-06-27 09:17:48 +0100

    adding x axis scale

--- charts/bubbleMatrix.js
+++ charts/bubbleMatrix.js
@@ -16,43 +16,44 @@
     var color = tree.dimension('color')
        .title('Color')
 
-    var xAxis = tree.dimension('xAxis')
-        .title("X Axis")
-        .types(String)
+
 
 
     var label = tree.dimension('label')
-       .title('Label')
-       .multiple(true)
+       .title('X Axis and Label')
+       
 
 	tree.map(function (data){
-      var root = { children : [] };
+      var root = { children : [], xdata:[] };
       //assume that there is 
 
-
+      var labels = d3.set([]);
       data.forEach(function (d){
 
         if (!hierarchy()) return root;
 
         var leaf = seek(root, hierarchy(d), hierarchy());
         if(leaf === false || !leaf) return;
+        //We have got to the bottom of the Y axis tree, now we must push this data point to the x axis data for this leaf
+        if (!leaf.xdata) leaf.xdata = [];
+        var xdatum = {};
 
-        if (!leaf.size) leaf.size = 0;
-        leaf.size += size() ? +size(d) : 1;
+        xdatum.size = size() ? +size(d) : 0;
 
-        leaf.color = color(d);
-        leaf.label = label(d);
-
+        xdatum.color = color(d);
+        xdatum.label = label(d);
+        leaf.xdata.push(xdatum);
+        labels.add(label(d));
         delete leaf.children;
       });
-     
 
-     
-      // Now we can iterate the nodes to find those with the bottom hierarchy and then rename the children
+
+
+      root.labels = labels.values();
+
       return root;
 
     });
-
 
 
 
@@ -95,13 +96,20 @@
 	var colors = chart.color()
 		.title("Color scale");
 
+	
+
 	chart.draw(function (selection, data){
 //tree is the right data model - the last node in the tree is the Y axis
+	
+var xScale = d3.scale.ordinal().domain(data.labels).rangePoints([0, width()/2], 0);
 	var g = selection
             .attr("width", +width() )
             .attr("height", +height() )
             .append("g")
                 .attr("transform", "translate(40,0)");
+
+
+
 
         var layout = d3.layout.tree()
             .size([+height(), +width()/2]);
@@ -127,11 +135,7 @@
                 .attr("class", "node")
                 .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
 
-        node.append("circle")
-            .style("fill", "#eeeeee")
-            .style("stroke","#999999")
-            .style("stroke-width","1px")
-            .attr("r", 4.5);
+        
 
         node.append("text")
             //.attr("dx", function(d) { return d.children ? -8 : -8; })
@@ -143,6 +147,43 @@
             .attr("text-anchor", "end" )
             .text(function (d){ return d.name; });
 
+            console.log(xScale("ET: The Extra-Terrestrial"));
+        node.each(function (d,i){
+        	d3.select(this).selectAll("circle").data(d.xdata).enter()
+        	.append("circle")
+        	//.attr("r",function (d){ return d.size; })
+        	.attr("r",10)
+        	.attr("transform", function(d) { return "translate(" + xScale(d.label) + "," + 0 + ")"; })
+
+        	;
+
+        });
+
+        node.append("circle")
+            .style("fill", "#eeeeee")
+            .style("stroke","#999999")
+            .style("stroke-width","1px")
+            .attr("r", 4.5);
+
+
+        // node.append(function (d){
+        // 	if (!d.xdata) {
+        // 		return "g";
+        // 	} else{
+        // 		a = document.createElement("g");
+        // 		var item = d3.select(a)
+        // 		.attr("width", +width() )
+        //     .attr("height", +height() );
+        // 		d.xdata.forEach(function(xdatum){
+        // 			item.append("circle").attr("r",function (datum){ return xdatum.size; });
+        // 		});
+
+        // 		return a;
+        		
+        // 	}
+
+        // 	});
+        
 
 	})
 
